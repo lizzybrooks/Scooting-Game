3ab3648d8c4dd007582966cf2e772f53a0c6b2fd
Frankie <efrankiemax@gmail.com>
2018-12-07 14:39:09 -0800

    Slowed Background and moved rectangle

--- game board/sketch.js
+++ game board/sketch.js
@@ -1,12 +1,20 @@
 function setup() {
   createCanvas(4*windowWidth, windowHeight);
+  background(random(150,250),random(45,65),random(220,244));
+
 }
 function draw() {
-  background(random(150,250),random(45,65),random(220,244));
+
+  if (frameCount % 30 == 0){
+    background(random(150,250),random(45,65),random(220,244));
+
+  }
+  gamePath();
 
 }
 
 function gamePath(){
   strokeWeight(1);
-  rect(-1, windowHeight-100, 200,101);
+  line(0,windowHeight/2,windowWidth,windowHeight/2)
+  rect(-1, 3*windowHeight/4-51, 150,51);
 }
